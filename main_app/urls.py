29afa61846b70aab75e852e492628a2ed6f94445
brandonb <brandontbyfield@gmail.com>
2025-07-11 17:42:07 -0400

    Views/Path Update

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,8 @@ from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('home/', views.home, name='home'),
+    path('profile/<int:profile_id>', views.profile_detail, name='profile-detail'),
+    path('guilds/', views.guild_index, name='guild-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-
+from .models import Profile, Guild
 # Import HttpResponse to send text-based responses
-from django.http import HttpResponse
+
 
 # Define the home view function
 def home(request):
     # Send a simple HTML response
-    return HttpResponse('<h1>Super Sweat</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def profile_detail(request, profile_id):
+    profile = Profile.objects.get(id=profile_id)
+    return render(request, 'detail.html', {'profile': profile})
+
+def guild_index(request, guild_id):
+    guild = Guild.objects.get(id=guild_id)
+    return render (request, 'index.html', {'guild': guild})

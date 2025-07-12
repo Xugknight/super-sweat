b263198448f47a566b85c0c7285da916818c86a2
skysalisbury <skysalisbury@gmail.com>
2025-07-12 10:37:29 -0400

    Model and Register along with CSS addition

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile, Guild, Membership, Event, EventTemplate, RSVP, ExternalAccount
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Guild)
+admin.site.register(Membership)
+admin.site.register(Event)
+admin.site.register(EventTemplate)
+admin.site.register(RSVP)
+admin.site.register(ExternalAccount)
